41a43a30b239996105fae6ed82be6387184857d1
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-06-11 16:35:55 -0300

    Create data.js

---
+++ Aulas/SiteTeste/BackEnd/data.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        id:"BXqUH86F-kA",
+        title: " Curo de Java Script ",
+        numeroAulas:"Introdução",
+        price:"FREE"
+    },
+    {
+        id:"uzEhd3Lugik",
+        title: " Curo de Java Script Módulo A",
+        numeroAulas:"4 Aulas",
+        price:"FREE"
+    },
+    {
+        id:"FjT97HVT5g8",
+        title: " Curo de Java Script Módulo B",
+        numeroAulas:"4 Aulas",
+        price:"FREE"
+    },
+    {
+        id:"H80nCKs9c2k",
+        title: " Curo de Java Script Módulo C",
+        numeroAulas:"2 Aulas",
+        price:"FREE"
+    },
+    {
+        id:"uPFasdmZHJc",
+        title: " Curo de Java Script Módulo D",
+        numeroAulas:"5 Aulas",
+        price:"FREE"
+    },
+    {
+        id:"3emz6rpcJyA",
+        title: " Curo de Java Script Módulo E",
+        numeroAulas:"5 Aulas",
+        price:"FREE"
+    },
+    {
+        id:"5m4UhZd-Les",
+        title: " Curo de Java Script Módulo F",
+        numeroAulas:"5 Aulas",
+        price:"FREE"
+    }
+]
