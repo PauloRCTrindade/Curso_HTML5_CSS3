d348d30837f61cb2e58f78762be9fa5d63d7c64e
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-06-11 16:36:01 -0300

    Update server.js

--- Aulas/SiteTeste/BackEnd/server.js
+++ Aulas/SiteTeste/BackEnd/server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 
-const server = express()
 
+const server = express()
+const videos = require("./data")
 server.use(express.static('public'))
 
-server.set('view engine','html')
+server.set('view engine','njk')
 
 nunjucks.configure('views',{
    express: server
@@ -16,7 +17,7 @@ server.get('/',function(req, res){
 })
 
 server.get('/videos',function(req,res){
-    return res.render('videos')
+    return res.render('videos',{itens:videos})
 })
 
 server.listen(5000,function(){
