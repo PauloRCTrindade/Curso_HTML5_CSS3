4452cd787393392981b79d22fad23961b7925e10
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-06-11 17:23:57 -0300

    Update server.js

--- Aulas/SiteTeste/BackEnd/server.js
+++ Aulas/SiteTeste/BackEnd/server.js
@@ -13,7 +13,18 @@ nunjucks.configure('views',{
 })
 
 server.get('/',function(req, res){
-    return res.render('sobre')
+    const sobre = {
+        imagemPerfil:"https://avatars0.githubusercontent.com/u/62801078?s=460&u=d4420dac3fa9e7a3cd184ca5f1a4831f78aa5b89&v=4",
+        nome:"Paulo R C Trindade",
+        atividade:"Estudante de Programação",
+        descricao:'"Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss deixa as pessoas mais interessantis. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Quem manda na minha terra sou euzis! A ordem dos tratores não altera o pão duris."',
+        links:[
+            {nome:"Git Hub", url:"https://github.com/PauloRCTrindade"},
+            {nome:"twitter", url:"https://github.com/PauloRCTrindade"},
+            {nome:"Instagran", url:"https://github.com/PauloRCTrindade"}
+        ]
+    }
+    return res.render('sobre',{sobre})
 })
 
 server.get('/videos',function(req,res){
